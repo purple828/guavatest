927db5b3ca79c8d98d1290a24dff3c13cb2f8283
fanglijuan <fanglijuan@baibu.la>
2018-11-29 10:59:17 +0800

    Initial commit

---
+++ src/main/java/com/github/flj/guavatest/GuavatestApplication.java
@@ -0,0 +1,12 @@
+package com.github.flj.guavatest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuavatestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GuavatestApplication.class, args);
+    }
+}
---
+++ src/main/java/com/github/flj/guavatest/controller/CollectionController.java
@@ -0,0 +1,25 @@
+package com.github.flj.guavatest.controller;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * <pre>
+ * ****************************************************
+ * Copyright BAIBU.
+ * All rights reserved.
+ * Description: ${添加描述}
+ * HISTORY:
+ * ***************************************************
+ *  Version      Date       Author       Desc
+ *   v1.0       2018/11/29     fanglijuan    ${添加描述}
+ *
+ * *********************************************
+ * </pre>
+ */
+@Controller
+public class CollectionController {
+
+    public void test(){
+        System.out.println("hello guava!");
+    }
+}
